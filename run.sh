ccfff85ef36bb4791a53c214c811b61c1520fe1c
Wen-Binghui <wbhwenbinghui@gmail.com>
2023-02-02 15:30:41 +0100

    runs ok

---
+++ run.sh
@@ -0,0 +1,5 @@
+build/bin/dso_dataset \
+		files=/home/wbh/datasets/tum_rgbd/rgbd_dataset_freiburg1_xyz \
+		calib=/home/wbh/datasets/tum_rgbd/rgbd_dataset_freiburg1_xyz/calib.txt \
+		preset=0 \
+		mode=1
