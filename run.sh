2feba1e75350b738d1ec7f2a9f7498d56f3123ec
Wen-Binghui <wbhwenbinghui@gmail.com>
2023-02-04 17:09:31 +0100

    cmake change first

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
+# build/bin/dso_dataset \
+# 		files=/home/wbh/datasets/tum_rgbd/rgbd_dataset_freiburg1_xyz \
+# 		calib=/home/wbh/datasets/tum_rgbd/rgbd_dataset_freiburg1_xyz/calib.txt \
+# 		preset=0 \
+# 		mode=1
+
+
 build/bin/dso_dataset \
-		files=/home/wbh/datasets/tum_rgbd/rgbd_dataset_freiburg1_xyz \
-		calib=/home/wbh/datasets/tum_rgbd/rgbd_dataset_freiburg1_xyz/calib.txt \
+		files=/home/wbh/datasets/backup/DemoDataSet/simulationSeq2 \
+		calib=/home/wbh/datasets/backup/DemoDataSet/calib.txt \
 		preset=0 \
 		mode=1
+
+
+
+
